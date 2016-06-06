a6dcb01d03f64c63b23d59d884d8c5bc1ec8c5d3
César Figueroa <cesar.figueroa@agilesolutions.com>
2016-06-06 12:03:07 -0600

    change example

--- src/floydwarshall/FloydWarshall.java
+++ src/floydwarshall/FloydWarshall.java
@@ -8,21 +8,21 @@ public class FloydWarshall {
     public static void main(String[] args) {
         P = new int[N][N];
         int[][] M = {
-            {0, 5, 999, 999},
-            {50, 0, 15, 5},
-            {30, 999, 0, 15},
-            {15, 999, 5, 0}
+            {0, 4, 2, 5},
+            {4, 0, 999, 1},
+            {2, 1, 0, 2},
+            {5, 1, 2, 0}
         };
 
         System.out.println("Matrix to find the shortest path of.");
         printMatrix(M);
         System.out.println("Shortest Path Matrix.");
-        printMatrix(FloydAlgo(M));
+        printMatrix(Floyd(M));
         System.out.println("Path Matrix");
         printMatrix(P);
     }
 
-    public static int[][] FloydAlgo(int[][] M) {
+    public static int[][] Floyd(int[][] M) {
         for (int k = 0; k < N; k++) {
             for (int i = 0; i < N; i++) {
                 for (int j = 0; j < N; j++) {
